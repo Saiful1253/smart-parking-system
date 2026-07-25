5fe9d40d6b3c08c8c8538650d45255c5ab052aa1
Saiful Islam Tanvin <tanvinislam3273@gmail.com>
2026-07-25 23:46:01 +0600

    feat: add Render deployment config and GitHub Pages CORS origin

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ console.log('ADMIN_KEY:', process.env.ADMIN_KEY ? 'loaded' : 'missing');
 connectDB();
 
 app.use(cors({
-    origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:8080', 'http://127.0.0.1:8080', 'file://'],
+    origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:8080', 'http://127.0.0.1:8080', 'file://', 'https://saiful1253.github.io'],
     credentials: true
 }));
 app.use(express.json());
