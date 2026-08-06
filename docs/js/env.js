a28ce98b297147591e27de8d9a97aee086aacb31
Saiful Islam Tanvin <tanvinislam3273@gmail.com>
2026-08-07 01:28:46 +0600

    security: update OAuth client ID after rotation

--- docs/js/env.js
+++ docs/js/env.js
@@ -1,5 +1,5 @@
 window.env = {
-  GOOGLE_CLIENT_ID: 'YOUR_GOOGLE_CLIENT_ID',
+  GOOGLE_CLIENT_ID: '348562821543-cde55r6i2mqfcbk4uishcctc1kcmssv.apps.googleusercontent.com',
   FIREBASE_CONFIG: {
     apiKey: "YOUR_API_KEY",
     authDomain: "smart-parking-system-df1f0.firebaseapp.com",
